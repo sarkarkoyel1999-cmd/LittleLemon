096f9d9bab9eaf872225fbaf94174d5523df9047
Koyel Sarkar <sarkarkoyel1999@gmail.com>
2026-07-10 00:07:28 +0530

    Set Up the Menu and Booking APIs

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
